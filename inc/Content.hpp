5e3dccf6994e7f4ffb71dd19d9dd57ae6e8bc7bc
BaiYe1975 <1975337015@qq.com>
2023-12-19 23:39:39 +0800

    更新：融合内容和时间。

--- inc/Content.hpp
+++ inc/Content.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>  
+#include "TimeHelper.hpp"
 
 using namespace std;
 
@@ -9,34 +11,46 @@ const int Maxvalue = 1000;
 class Informa {
 private:
     string infor;
+    TimeInfo Time;
 
 public:
-    Informa(string _infor="") {
+    Informa(string _infor = "", int _year = 0, int _month = 0, int _day = 0, int _hour = 0, int _min = 0, int _sec = 0) {
         infor = _infor;
+        Time.year = _year;
+        Time.month = _month;
+        Time.day = _day;
+        Time.hour = _hour;
+        Time.minute = _min;
+        Time.second = _sec;
     }
 
     string getInfo() const {
         return infor;
+    }
+
+    TimeInfo getTime() const {
+        return Time;
     }
 };
 
 class Agenda {
 private:
     int size;
-    Informa elements[Maxvalue];
+    Informa* elements; // 使用指针
     string fileName; // 日程表对应的文件名
 
 public:
     // 构造函数，创建对象时新建文档
     Agenda(string _fileName) : fileName(_fileName) {
         size = 0;
-        loadFromFile();  // 从文件加载信息到日程表
+        elements = new Informa[Maxvalue]; // 动态分配内存
+        loadFromFile(); // 从文件加载信息到日程表
     }
 
     ~Agenda() {
+        delete[] elements; // 释放内存
         saveToFile(); // 保存信息到文件
     }
-
     // 添加信息到日程表
     void addInforma(const Informa& info) {
         if (size < Maxvalue) {
@@ -74,12 +88,36 @@ public:
         return -1; // 未找到信息
     }
 
-    // 显示日程表中的所有信息
+    // 按时间查找信息并返回其索引，如果不存在返回-1
+    int findInformaByTime(const TimeInfo& time) const {
+        for (int i = 0; i < size; ++i) {
+            if (compareTimes(elements[i].getTime(), time) == 0) {
+                return i;
+            }
+        }
+        return -1; // 未找到信息
+    }
+
+    // 显示日程表中的所有信息，包括时间
     void displayInforma() const {
         cout << "日程表中的信息：" << endl;
         for (int i = 0; i < size; ++i) {
-            cout << "[" << i << "] " << elements[i].getInfo() << endl;
+            cout << "[" << i << "] " << elements[i].getInfo()
+                << " 时间：" << elements[i].getTime().year << "-"
+                << elements[i].getTime().month << "-"
+                << elements[i].getTime().day << " "
+                << elements[i].getTime().hour << ":"
+                << elements[i].getTime().minute << ":"
+                << elements[i].getTime().second << endl;
         }
+    }
+
+    // 获取指定索引的信息
+    string getInfoByIndex(int index) const {
+        if (index >= 0 && index < size) {
+            return elements[index].getInfo();
+        }
+        return "无效的索引";
     }
 
 private:
@@ -87,27 +125,52 @@ private:
     void loadFromFile() {
         ifstream inputFile(fileName);
         if (inputFile.is_open()) {
-            while (!inputFile.eof() && size < Maxvalue) {
-                string info;
-                getline(inputFile, info);
-                if (!info.empty()) {
-                    elements[size++] = Informa(info);
+            string info; // 添加此行
+            while (size < Maxvalue && getline(inputFile, info)) {
+                // 使用stringstream解析时间和事件信息
+                istringstream iss(info);
+                string eventInfo;
+                int year, month, day, hour, min, sec;
+
+                // 尝试解析一行信息
+                if (!(iss >> eventInfo >> year >> month >> day >> hour >> min >> sec)) {
+                    cout << "无效的行: " << info << endl;
+                    continue;
                 }
+
+                elements[size++] = Informa(eventInfo, year, month, day, hour, min, sec);
             }
             inputFile.close();
         }
     }
+
+
+
 
     // 保存信息到文件
     void saveToFile() const {
         ofstream outputFile(fileName);
         if (outputFile.is_open()) {
             for (int i = 0; i < size; ++i) {
-                outputFile << elements[i].getInfo() << endl;
+                outputFile << elements[i].getInfo() << " "
+                    << elements[i].getTime().year << " "
+                    << elements[i].getTime().month << " "
+                    << elements[i].getTime().day << " "
+                    << elements[i].getTime().hour << " "
+                    << elements[i].getTime().minute << " "
+                    << elements[i].getTime().second << endl;
             }
             outputFile.close();
         }
     }
+
+    // 比较两个时间是否相等
+    int compareTimes(const TimeInfo& time1, const TimeInfo& time2) const {
+        if (time1.year != time2.year) return time1.year - time2.year;
+        if (time1.month != time2.month) return time1.month - time2.month;
+        if (time1.day != time2.day) return time1.day - time2.day;
+        if (time1.hour != time2.hour) return time1.hour - time2.hour;
+        if (time1.minute != time2.minute) return time1.minute - time2.minute;
+        return time1.second - time2.second;
+    }
 };
-
-
--- src/Content.cpp
+++ src/Content.cpp
@@ -1,59 +1,80 @@
-#include "content.hpp"
+#include <iostream>
+#include "Content.hpp"
 
 int main() {
-    // 创建日程表并指定文件名，会在创建对象时新建文档
-    Agenda myAgenda("agenda.txt");
+    // 创建 Agenda 对象，文件名为 "agenda.txt"
+    Agenda agenda("agenda.txt");
 
     while (true) {
-        cout << "请选择操作:" << endl;
-        cout << "1. 添加日程" << endl;
-        cout << "2. 显示日程" << endl;
-        cout << "3. 删除日程" << endl;
-        cout << "4. 查找日程" << endl;
-        cout << "0. 退出" << endl;
+        cout << "选择操作：" << endl;
+        cout << "1. 添加信息" << endl;
+        cout << "2. 查找信息" << endl;
+        cout << "3. 显示所有信息" << endl;
+        cout << "4. 退出" << endl;
 
         int choice;
         cin >> choice;
 
         switch (choice) {
         case 1: {
-            cout << "请输入要添加的日程信息：";
+            cout << "输入信息：" << endl;
             string info;
-            cin.ignore(); // 忽略前面的换行符
+            cin.ignore(); // Clear the newline from the buffer
             getline(cin, info);
-            myAgenda.addInforma(Informa(info));
+
+            cout << "输入时间：" << endl;
+            int year, month, day, hour, min, sec;
+            cout << "年: ";
+            cin >> year;
+            cout << "月: ";
+            cin >> month;
+            cout << "日: ";
+            cin >> day;
+            cout << "时: ";
+            cin >> hour;
+            cout << "分: ";
+            cin >> min;
+            cout << "秒: ";
+            cin >> sec;
+
+            Informa infoToAdd(info, year, month, day, hour, min, sec);
+            agenda.addInforma(infoToAdd);
             break;
         }
-        case 2:
-            myAgenda.displayInforma();
-            break;
-        case 3: {
-            cout << "请输入要删除的日程的索引：";
-            int indexToDelete;
-            cin >> indexToDelete;
-            myAgenda.removeInforma(indexToDelete);
-            break;
-        }
-        case 4: {
-            cout << "请输入要查找的日程信息：";
-            string infoToFind;
-            cin.ignore(); // 忽略前面的换行符
-            getline(cin, infoToFind);
-            Informa info(infoToFind);
-            int foundIndex = myAgenda.findInforma(info);
-            if (foundIndex != -1) {
-                cout << "找到信息在索引 " << foundIndex << " 处。" << endl;
+        case 2: {
+            cout << "输入要查找的时间：" << endl;
+            int year, month, day, hour, min, sec;
+            cout << "年: ";
+            cin >> year;
+            cout << "月: ";
+            cin >> month;
+            cout << "日: ";
+            cin >> day;
+            cout << "时: ";
+            cin >> hour;
+            cout << "分: ";
+            cin >> min;
+            cout << "秒: ";
+            cin >> sec;
+
+            TimeInfo searchTime = { year, month, day, hour, min, sec };
+            int index = agenda.findInformaByTime(searchTime);
+            if (index != -1) {
+                cout << "找到信息 [" << index << "]: " << agenda.getInfoByIndex(index) << endl;
             }
             else {
-                cout << "未找到信息。" << endl;
+                cout << "未找到匹配时间的信息。" << endl;
             }
             break;
         }
-        case 0:
-            cout << "程序退出。" << endl;
+        case 3:
+            agenda.displayInforma();
+            break;
+        case 4:
+            cout << "程序已退出。" << endl;
             return 0;
         default:
-            cout << "无效的选择，请重新输入。" << endl;
+            cout << "无效的选择。请重新选择操作。" << endl;
         }
     }
 
